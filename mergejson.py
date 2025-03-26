0c80a41dbd7642931aa556956225e556798fbc4f
jpanimboza <61572277+jpanimboza@users.noreply.github.com>
2025-03-26 12:55:44 -0500

    Update mergejson.py

--- mergejson.py
+++ mergejson.py
@@ -1,10 +1,8 @@
 import json
 import glob
-
 result = []
 for f in glob.glob("Incendios/*.json"):
     with open(f, "r") as infile:
         result.append(json.load(infile))
-
 with open("merged_file.json", "wb") as outfile:
-     json.dump(result, outfile)
+    json.dump(result, outfile)
