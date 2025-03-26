17f4e850242f861da799d616ea13f13dc342f3af
jpanimboza <61572277+jpanimboza@users.noreply.github.com>
2025-03-26 13:18:44 -0500

    Update mergejson.py

--- mergejson.py
+++ mergejson.py
@@ -1,10 +1,9 @@
 import json
 import glob
 result = []
-print(glob.glob("Incendios/*.geojson"))
 for f in glob.glob("Incendios/*.geojson"):
     print(f)
     with open(f, "r") as infile:
         result.append(json.load(infile))
-with open("merged_file.json", "wb") as outfile:
+with open("Incendios/merged_file.geojson", "wb") as outfile:
     json.dump(result, outfile)
