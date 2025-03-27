e14ca0ee3a2acaeed745e8be24274e20b5a75bfb
jpanimboza <61572277+jpanimboza@users.noreply.github.com>
2025-03-27 10:46:05 -0500

    Update mergejson.py

--- mergejson.py
+++ mergejson.py
@@ -8,7 +8,6 @@ for f in glob.glob("Incendios/*.geojson"):
     with open(f, "rb") as infile:
         layer = geojson.load(infile)
         feature_i = layer['features']
-        print(Feature(feature_i).get("type"))
         collection.append(feature_i)
 geo_collection = geojson.FeatureCollection(collection)
 print(geo_collection)
